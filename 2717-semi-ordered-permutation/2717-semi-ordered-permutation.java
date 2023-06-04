dbd75839f006940f60bea627a621912984a9392e
swati2101 <61512107+swati2101@users.noreply.github.com>
2023-06-04 10:44:49 +0530

    Time: 13 ms (100.00%), Space: 43.7 MB (20.00%) - LeetHub

---
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int semiOrderedPermutation(int[] nums) {
+        int n=nums.length;
+        if(nums[0]==1 && nums[n-1]==n)
+            return 0;
+        if(n==2){
+            return 1;
+        }
+        int first=0,last=0;
+        for(int i = 0; i < n; ++i){
+            if(nums[i] == 1) first = i;
+            if(nums[i] == n) last = i;
+        }
+        System.out.println(first+" "+last);
+        //System.out.println()
+        /*int count1=0;
+        if(first!=0){
+        for(int i=first;i>=1;i--){
+                count1++;
+                int temp=nums[i];
+                nums[i]=nums[i-1];
+                nums[i-1]=temp;
+            System.out.println(i);
+        }
+        }
+        /*while(first!=1){
+            int temp=nums[first];
+            nums[first]=nums[first-1];
+            nums[first-1]=temp;
+            first--;
+            count1++;
+        }*/
+        
+        /*int count2=0;
+        if(last!=n-1){
+        for(int i=last;i<n-1;i++){
+            count2++;
+            int temp=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
+            System.out.println(i);
+        }
+        }
+        /*while(last!=n-2){
+            int temp=nums[last];
+            nums[last]=nums[last+1];
+            nums[last+1]=temp;
+            last++;
+            count2++;
+        }*/
+        //return count1+count2;
+        int count=0;
+        if(first<last)
+            count=first+(n-1-last);
+        if(first>last)
+            count=first+(n-1-last)-1;
+        return count;
+    }
+}
