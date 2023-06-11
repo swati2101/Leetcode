8f94feafbd3623a11050135fbfef6394efba0272
swati2101 <61512107+swati2101@users.noreply.github.com>
2023-06-11 12:56:37 +0530

    Time: 71 ms (38.57%), Space: 88.2 MB (11.00%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.java
@@ -0,0 +1,31 @@
+class SnapshotArray {
+ArrayList<TreeMap<Integer,Integer>> arr;
+    int ind;
+    public SnapshotArray(int length) {
+        arr=new ArrayList<>();
+        for(int i=0;i<length;i++){
+            arr.add(i,new TreeMap<Integer,Integer>());
+            arr.get(i).put(0,0);
+        }
+    }
+    
+    public void set(int index, int val) {
+        arr.get(index).put(ind,val);
+    }
+    
+    public int snap() {
+        return ind++;
+    }
+    
+    public int get(int index, int snap_id) {
+        return arr.get(index).floorEntry(snap_id).getValue();
+    }
+}
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.set(index,val);
+ * int param_2 = obj.snap();
+ * int param_3 = obj.get(index,snap_id);
+ */
